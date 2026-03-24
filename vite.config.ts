4f922f45cab38b55c6d5e17a9418acc0fd0428f2
geamnegru <gamsti69420@gmail.com>
2026-03-24 23:37:50 +0200

    config changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    headers: {
-      'Cross-Origin-Opener-Policy': 'same-origin',
-      'Cross-Origin-Embedder-Policy': 'require-corp',
+    server: {
+      headers: {
+        'Cross-Origin-Opener-Policy': 'same-origin',
+      },
     },
-  },
-  preview: {
-    headers: {
-      'Cross-Origin-Opener-Policy': 'same-origin',
-      'Cross-Origin-Embedder-Policy': 'require-corp',
+    preview: {
+      headers: {
+        'Cross-Origin-Opener-Policy': 'same-origin',
+      },
     },
-  },
 })
